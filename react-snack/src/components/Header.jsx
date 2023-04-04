27434446e8d98d2355eea9d39632de57e831ad0f
Viktor <vitya.abrosimov19@gmail.com>
2023-04-04 15:32:53 +0300

    refreshed logo

--- react-snack/src/components/Header.jsx
+++ react-snack/src/components/Header.jsx
@@ -5,34 +5,33 @@ import { changeFilterBy, filterBy } from "../features/snackSlice";
 import FilterBySnack from "../subComponents/FilterBySnack";
 
 export default function Header({ total }) {
-  const filterBySnacks = useSelector(filterBy);
-  const dispath = useDispatch();
+    const filterBySnacks = useSelector(filterBy);
+    const dispath = useDispatch();
 
-  function handleChangeFilterSnacks(filter) {
-    dispath(changeFilterBy(filter));
-  }
+    function handleChangeFilterSnacks(filter) {
+        dispath(changeFilterBy(filter));
+    }
 
-  return (
-    <div className="header__container">
-      <div className="header__logo">
-        <img
-          width="46"
-          src="https://react-pizza-project.herokuapp.com/static/media/pizza-logo.f3327011.svg"
-          alt="Pizza logo"
-        />
-        <h1>REACT-SNACK</h1>
-        <span>Время перекуса!</span>
-      </div>
-      <div className="header__cart">
-        <Link to="/cart">
-          <button className="button--cart button">
-            <span>КОРЗИНА {total}₽</span>
-          </button>
-        </Link>
-      </div>
-      <div className="header__filter">
-        <FilterBySnack filterBySnacks={filterBySnacks} handleChangeFilterSnacks={handleChangeFilterSnacks} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="header__container">
+            <div className="header__logo">
+                <img
+                    src="https://img.icons8.com/external-icongeek26-flat-icongeek26/60/null/external-pizza-mexican-food-icongeek26-flat-icongeek26.png"
+                    alt="pizza_logo"
+                />
+                <h1>REACT-SNACK</h1>
+                <span>Время перекуса!</span>
+            </div>
+            <div className="header__cart">
+                <Link to="/cart">
+                    <button className="button--cart button">
+                        <span>КОРЗИНА {total}₽</span>
+                    </button>
+                </Link>
+            </div>
+            <div className="header__filter">
+                <FilterBySnack filterBySnacks={filterBySnacks} handleChangeFilterSnacks={handleChangeFilterSnacks} />
+            </div>
+        </div>
+    );
 }
